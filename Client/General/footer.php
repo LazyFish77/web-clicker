f11bfe1f32047d45354a400c17462c3cab3e38d2
Tyler Fischer <fischt77@uwosh.edu>
2017-11-14 09:48:48 -0600

    removed header and body tags from footer and nav snippets

--- Client/General/footer.php
+++ Client/General/footer.php
@@ -1,25 +1,13 @@
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="description" content="Footer for all pages">
-        <meta name="keywords" content="webclicker homepage">
-        <meta name="author" content="Tyler Fischer">
-        <title>Web Clicker</title>
-    </head>
-    <body>
-        <footer id="footer">
-            <div>
-                <a href="http://jigsaw.w3.org/css-validator/check/referer">
-                    <img class="footerimage" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!">
-                </a>
-            </div>
-            <div>
-                <img class="footerimage" src="http://webdev.cs.uwosh.edu/WebCLICKER/public/images/html5_logo.png" alt="\'Valid\' HTML5">
-            </div>
-            <div>
-                <span id="footertext">2017- Univ of Wisconsin Oshkosh</span>
-            </div>
-        </footer>
-    </body>
-</html>
+<footer id="footer">
+    <div>
+        <a href="http://jigsaw.w3.org/css-validator/check/referer">
+            <img class="footerimage" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!">
+        </a>
+    </div>
+    <div>
+        <img class="footerimage" src="http://webdev.cs.uwosh.edu/WebCLICKER/public/images/html5_logo.png" alt="\'Valid\' HTML5">
+    </div>
+    <div>
+        <span id="footertext">2017- Univ of Wisconsin Oshkosh</span>
+    </div>
+</footer>
--- Client/General/student-nav.php
+++ Client/General/student-nav.php
@@ -1,8 +1,9 @@
 <div id="nav">
-            <h1 id="navheader">Web Clicker
-                <a href="../Student/next-question.php">Next question</a>
-                <a href="../Student/view-old-question.php">Review</a>
-                <a href="./reset-password.php">Reset password</a>
-                <a href="../General/login-page.php">Log out</a>
-            </h1>
-        </div>
+    <h1 id="navheader">
+        <label>Web Clicker</label>
+        <a href="../Student/next-question.php">Next question</a>
+        <a href="../Student/view-old-question.php">Review</a>
+        <a href="./reset-password.php">Reset password</a>
+        <a href="../General/login-page.php">Log out</a>
+    </h1>
+</div>
